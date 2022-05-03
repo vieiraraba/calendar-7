5b3f063aa2b1d8d89a001c008845fa9d4e115ed2
Ricardo Vieira <vieiraraba@gmail.com>
2022-05-03 10:23:54 +0200

    testing-zindex

--- js/script.js
+++ js/script.js
@@ -1,52 +1,70 @@
-// const monthsOf2002 = [
-//   [2022, 1],
-//   [2022, 2],
-//   [2022, 3],
-//   [2022, 4],
-//   [2022, 5],
-//   [2022, 6],
-//   [2022, 7],
-//   [2022, 8],
-//   [2022, 9],
-//   [2022, 10],
-//   [2022, 11],
-//   [2022, 12],
-// ];
-
-const getDaysArray = (year, month) => {
-    const monthIndex = month - 1;
-    const names = [
-    'SUN',
-    'MON',
-    'TUE',
-    'WED',
-    'THU',
-    'FRI',
-    'SAT',
-    ];
-    const date = new Date(year, monthIndex, 1);
-    const result = [];
-    while (date.getMonth() === monthIndex) {
-    result.push(date.getDate() + ' ' + names[date.getDay()]);
-    date.setDate(date.getDate() + 1);
-    }
-    return result;
+const weekdaysIndexes = {
+    Monday: 0,
+    Tuesday: 1,
+    Wednesday: 2,
+    Thursday: 3,
+    Friday: 4,
+    Saturday: 5,
+    Sunday: 6,
 };
 
-const array = getDaysArray(2022, 5);
-const createMonthLayout = (day) => {
+const userEvents = [];
+
+const dateObj = new Date();
+const day = dateObj.getDay();
+const month = dateObj.getMonth();
+const year = dateObj.getFullYear();
+const formInputs = document.querySelectorAll('form input');
+const formSelect = document.querySelector('form select');
+const openEventModal = (e) => {};
+const addUserEvent = (e) => {
+    e.preventDefault();
+
+    const userEvent = {
+    title: formInputs[0].value,
+    location: formInputs[1].value,
+    startDate: formInputs[3].value,
+    startTime: formInputs[4].value,
+    endDate: formInputs[4].value,
+    endTime: formInputs[5].value,
+    eventType: formSelect.value,
+    };
+    console.log(userEvent);
+};
+
+document.getElementById('save-btn').addEventListener('click', addUserEvent);
+
+const onLoad = () => {
+    const sumDaysOfCurrentMonth = new Date(year, month + 1, 0).getDate();
+    const firstDayOfCurrentMonth = new Date(year, month, 1);
+    const localesOptions = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+    };
+    const dateToString = firstDayOfCurrentMonth.toLocaleDateString(
+    'en-GB',
+    localesOptions
+    );
+    const firstDayString = dateToString.split(',')[0];
+    const firstDayindex = weekdaysIndexes[firstDayString];
+    for (let i = 1; i <= firstDayindex + sumDaysOfCurrentMonth; i++) {
     const monthEl = document.querySelector('.month-wrapper');
     const dayEl = document.createElement('div');
-    const h3El = document.createElement('h3');
-    h3El.innerText = day;
-    h3El.className = 'month-day-title';
+    const pEl = document.createElement('p');
     dayEl.className = 'month-day-view';
     monthEl.appendChild(dayEl);
-    dayEl.appendChild(h3El);
+
+    if (i > firstDayindex) {
+        pEl.textContent = i - firstDayindex;
+        dayEl.appendChild(pEl);
+        dayEl.id = i - firstDayindex;
+        dayEl.addEventListener('click', openEventModal);
+    }
+    }
 };
 
-array.forEach((day) => {
-    createMonthLayout(day);
-    // console.log(day);
-});
+onLoad();
+
 
